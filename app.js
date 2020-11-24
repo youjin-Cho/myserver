7f794fea241b46d016b068c58a522368778d5212
youjin-Cho <ud071233@gmail.com>
2020-11-24 21:04:52 +0900

    app.js 파일 수정
    form.html 파일에서 전송한 email이 DB에 저장되어 있는지 여부에 따른 responseData를 응답으로 전송한다.

--- app.js
+++ app.js
@@ -44,9 +44,16 @@ app.post('/ajax_send_email', function(req, res) {
   var email = req.body.email;
   var responseData = {};
 
-  var query = connection.query(`select name from user where email= ${email}`, function(err, rows) {
-
+  var query = connection.query(`select name from user where email= '${email}'`, function(err, rows) {
+    if(err) throw err;
+    if(rows[0]) {
+      responseData.result = "ok";
+      responseData.name = rows[0].name;
+    }else {
+      responseData.result = "none";
+      responseData.name = "";
+    }
+    res.json(responseData);
   })
 
-  res.json(responseData);
 })
