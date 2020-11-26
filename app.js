ed056b4318177a5b7168ccc17e39c8ca4028cae5
youjin-Cho <ud071233@gmail.com>
2020-11-26 19:56:24 +0900

    /main 경로 라우팅
    /main 경로로 요청이 들어올 경우 public 폴더의 main.html 파일을 클라이언트에게 전송한다.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var mysql = require('mysql');
+var main = require('./router/main');
 
 var connection = mysql.createConnection({
   host : 'localhost',
@@ -25,15 +26,15 @@ app.listen(3000, function(){
 });
 
 app.use(express.static('public'))
+// express에서 JSON Ruquest Body 파싱
+// body-parser는 4.16 이전 버전에서 사용
 app.use(express.json())
 //app.use(cors())
 app.use(express.urlencoded({extended:true}))
 app.set('view engine', 'ejs')
 //app.set('views', 'views')
 
-app.get('/', function(req, res) {
-  res.sendFile(__dirname + "/public/main.html");
-})
+app.use('/main', main)
 
 app.post('/email_post', function(req, res) {
   //res.send(req.body.email)
--- router/main.js
+++ router/main.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 var router = express.Router();
 
-router.get('/main', function(req, res) {
-  res.sendFile(__dirname + '/public/main.html')
+router.get('/', function(req, res) {
+  console.log('main.js loaded')
+  res.sendFile(path.join(__dirname,'/../public/main.html'))
 });
 
 module.exports = router;
